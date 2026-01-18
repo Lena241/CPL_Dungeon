e487f4cd1318a8fd7c703b1ea8c04040645b8e04
Steffen Braun <steffen.braun@iem.fraunhofer.de>
2026-01-18 10:38:56 +0100

    Added handling of variable assignments and expression processing to the AstBuilder.

--- blockly/src/dsl/AstBuilder.java
+++ blockly/src/dsl/AstBuilder.java
@@ -40,6 +40,69 @@ public class AstBuilder extends dsl.DungeonDSLBaseVisitor<Object> {
     return new ShootFireballStmt();
   }
 
+  public Object visitSetVariableStmt(dsl.DungeonDSLParser.SetVariableStmtContext ctx){
+    String variable_name = ctx.VAR_NAME().getText();
+    ExpressionStmt value = (ExpressionStmt) this.visitExpressionRootStmt(ctx.expressionRoot());
+
+    return new SetVariableStmt(variable_name, value);
+  }
+
+  public Object visitExpressionRootStmt(dsl.DungeonDSLParser.ExpressionRootContext ctx){
+
+
+    if (ctx.expressionFirstOrder() != null) {
+      return (ExpressionStmt) this.visitExpressionFirstOrderStmt(ctx.expressionFirstOrder());
+    } else {
+      throw new IllegalArgumentException("Expression without value!");
+    }
+  }
+
+  public Object visitExpressionFirstOrderStmt(dsl.DungeonDSLParser.ExpressionFirstOrderContext ctx){
+
+
+    if (ctx.expressionFirstOrder().size() == 2) {
+      List<dsl.DungeonDSLParser.ExpressionFirstOrderContext> expressionSet = ctx.expressionFirstOrder();
+      ExpressionStmt leftInput = (ExpressionStmt) this.visitExpressionFirstOrderStmt(expressionSet.get(0));
+      ExpressionStmt rightInput = (ExpressionStmt) this.visitExpressionFirstOrderStmt(expressionSet.get(1));
+      return new ExpressionStmt(leftInput, rightInput, ctx.FIRST_ORDER_OPERATOR().getText());
+    } else if (ctx.expressionSecondOrder() != null) {
+      return (ExpressionStmt) this.visitExpressionSecondOrderStmt(ctx.expressionSecondOrder());
+    } else {
+      throw new IllegalArgumentException("Expression without value!");
+    }
+  }
+
+  public Object visitExpressionSecondOrderStmt(dsl.DungeonDSLParser.ExpressionSecondOrderContext ctx){
+
+
+    if (ctx.expressionSecondOrder().size() == 2) {
+      List<dsl.DungeonDSLParser.ExpressionSecondOrderContext> expressionSet = ctx.expressionSecondOrder();
+      ExpressionStmt leftInput = (ExpressionStmt) this.visitExpressionSecondOrderStmt(expressionSet.get(0));
+      ExpressionStmt rightInput = (ExpressionStmt) this.visitExpressionSecondOrderStmt(expressionSet.get(1));
+      return new ExpressionStmt(leftInput, rightInput, ctx.SECOND_ORDER_OPERATOR().getText());
+    } else if (ctx.expressionLeaf() != null) {
+      return (ExpressionStmt) this.visitExpressionLeafStmt(ctx.expressionLeaf());
+    } else {
+      throw new IllegalArgumentException("Expression without value!");
+    }
+  }
+
+  public Object visitExpressionLeafStmt(dsl.DungeonDSLParser.ExpressionLeafContext ctx){
+
+
+    if (ctx.VAR_NAME() != null) {
+      String variable_name = ctx.VAR_NAME().getText();
+      return new ExpressionStmt(variable_name, true);
+    } else if (ctx.INT() != null) {
+      String value = ctx.INT().getText();
+      return new ExpressionStmt(value, false);
+    } else if (ctx.expressionRoot() != null) {
+       return (ExpressionStmt) this.visitExpressionRootStmt(ctx.expressionRoot());
+    } else {
+      throw new IllegalArgumentException("Expression without value!");
+    }
+  }
+
   @Override
   public Object visitPickupStmt(dsl.DungeonDSLParser.PickupStmtContext ctx) {
     return new PickupStmt();
